a7230489e39385126f82c06ff27a1a8faaf77239
Samuel Hafen <samuel.hafen@aol.com>
2026-03-09 15:24:28 +0100

    prototyping room creating and event streaming

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # 1-gegen-100
 
 Party game variant of the 1 vs 100 gameshow.
+
+## Round Logic
+
+```mermaid
+flowchart LR
+    
+    subgraph Round
+
+        spieler-auswahl(Player selection)
+
+
+        --> question(Question is Asked)
+        --> gegner-antwort(Contestans Answer)
+        --> spieler-antwort(Player answers)
+        --> gegner-ausscheiden(Contestants Loose)
+        --> gegner-uebrig{Contestants > 0?}
+    
+        gegner-uebrig -- no --> spieler-risiko{Player wants resolution}
+        spieler-risiko -- yes --> spieler-richtig
+
+        gegner-uebrig -- yes --> spieler-richtig{Players answer correct?}
+
+        spieler-richtig -- yes --> win(Get points from Loosers)
+        --> frage-ende{question == 10 or contestants == 0}
+
+        frage-ende -- yes --> round-end
+        frage-ende -- no --> question
+
+
+        spieler-richtig -- no --> loose(Loose all points)
+        --> round-end
+
+    end
+
+
+```
--- src/main.rs
+++ src/main.rs
@@ -8,38 +8,55 @@ use rocket::Request;
 use rocket::http::Cookie;
 use rocket::http::SameSite;
 
+use rocket::fairing::AdHoc;
+use rocket::response::stream::{EventStream, Event};
+use rocket::serde::json::Json;
+use rocket::tokio::select;
+use rocket::tokio::sync::broadcast;
+use rocket::{get, post, routes, Build, Rocket, State};
+
 use serde::{Serialize, Deserialize};
+
+// Mutable state
+use std::sync::RwLock;
+
+// Unique room id
+use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Room {
-    id: String,
-    name: String,
+    users: Vec<User>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct User {
     id: String,
     name: String,
-    room_id: Option<String>,
 }
 
 fn new_user() -> User {
     User {
         id: uuid::Uuid::new_v4().to_string(),
         name: "Username".to_string(),
-        room_id: None,
     }
 }
 
+// Implement a Request guard for User struct
 #[rocket::async_trait]
 impl<'r> FromRequest<'r> for User {
     type Error = ();
     async fn from_request(req: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+
+        // Check if a user cookie exists
         let cookie = req.cookies().get("user_token").map(|c| c.value().to_string());
         match cookie {
             Some(cookie) => {
                 match serde_json::from_str(&cookie) {
+
+                    // If the cookie is valid, return the user
                     Ok(user) => Outcome::Success(user),
+
+                    // If the cookie is invalid, create a new user
                     Err(_) => {
                         let user = new_user();
                         let new_cookie = Cookie::build(("user_token", serde_json::to_string(&user).unwrap()))
@@ -52,6 +69,7 @@ impl<'r> FromRequest<'r> for User {
                 }
             }
 
+            // If there is no cookie, create a new user
             None => {
                 let user = new_user();
                 let new_cookie = Cookie::build(("user_token", serde_json::to_string(&user).unwrap()))
@@ -72,25 +90,143 @@ struct Question {
     wrong2: String,
 }
 
-
-#[get("/room/list")]
-fn hello() -> String {
-    format!("Hello")
+fn room_exists(state: &State<AppState>, room_id: String) -> bool {
+    // Check for room id in hash map
+    state.rooms.read().unwrap().contains_key(&room_id)
 }
 
-#[get("/room/create")]
-fn create_room(user: User) -> String {
-    format!("Hello {}", user.name)
+
+#[get("/room/<room_id>/manager")]
+fn manage_room(state: &State<AppState>, room_id: String) -> EventStream![] {
+    
+    let room = Room {
+        users: Vec::new(),
+    };
+
+    // add room if not exists
+    match state.rooms.read().unwrap().get(&room_id) { 
+        Some(found_room) =>  (),
+        None => {
+            state.rooms.write().unwrap().insert(room_id.to_string(), RwLock::new(room));
+        },
+    }
+
+    let mut rx = state.tx.subscribe();
+    EventStream! {
+
+        loop {
+            let event = match rx.recv().await {
+                Ok(event) => event,
+                Err(_) => break,
+            };
+
+            if event.room_id == room_id {
+                yield Event::json(&event);
+            }
+            
+        }
+    }
+    
+}
+
+
+
+#[get("/room/<room_id>/exists")]
+fn check_room(state: &State<AppState>, room_id: String) -> String {
+
+    if room_exists(state, room_id) {
+        "true".to_string()
+    } else {
+        "false".to_string()
+    }
+}
+
+#[get("/room/<room_id>/player")]
+fn join_room(state: &State<AppState>, user: User, room_id: String) -> EventStream![] {
+    
+    let rooms = state.rooms.read().unwrap();
+    let room = rooms.get(&room_id).unwrap();
+
+    let mut found = false;
+    for u in room.read().unwrap().users.iter() {
+        if u.id == user.id {
+            found = true;
+            break;
+        }
+    }
+
+    if !found {
+        room.write().unwrap().users.push(user.clone());
+    }
+
+    let _ = state.tx.send(AppEvent {
+        room_id: room_id.clone(),
+        kind: EventKind::UserJoined { user: user.clone() },
+    });
+
+    let mut rx = state.tx.subscribe();
+    EventStream! {
+        loop {
+            let event = match rx.recv().await {
+                Ok(event) => event,
+                Err(_) => break,
+            };
+
+            if event.room_id == room_id {
+                yield Event::json(&event);
+            }
+            
+        }
+    }
+}
+
+#[get("/events")]
+fn listen_events(state: &State<AppState>) -> EventStream![] {
+    let mut rx = state.tx.subscribe();
+    EventStream! {
+        loop {
+            let event = match rx.recv().await {
+                Ok(event) => event,
+                Err(_) => break,
+            };
+
+            yield Event::json(&event);
+        }
+    }
+}
+
+#[get("/send_event")]
+fn test_event(state: &State<AppState>) {
+    let _ = state.tx.send(AppEvent{ room_id: "test".to_string(), kind: EventKind::UserJoined { user: new_user() } });
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct AppEvent {
+    room_id: String,
+    kind: EventKind,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+enum EventKind {
+    UserJoined { user: User },
+    UserLeft { user: User },
+}
+
+struct AppState {
+    tx: broadcast::Sender<AppEvent>,
+    rooms: RwLock<HashMap<String, RwLock<Room>>>,
 }
 
 #[launch]
 fn rocket() -> _ {
 
-
-    let mut rooms: Vec<Room>  = Vec::new();
+    let (tx, _rx) = broadcast::channel(1024);
 
     rocket::build()
-        .mount("/api", routes![hello, create_room])
+        .mount("/api", routes![listen_events, test_event, manage_room, join_room, check_room])
         .mount("/", FileServer::from(relative!("html")))
-        .manage(rooms)
+        .manage(AppState {
+            tx,
+            rooms: RwLock::new(HashMap::new()),
+        })
 }
